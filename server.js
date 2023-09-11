0d2d08685dc840ebe5dfb742b8ebac3ac713907e
swati-anchatageri <swatianchatageri129@gmail.com>
2023-09-12 00:31:21 +0530

    mock

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const JsonServer=require("json-server"); // Importing son server library
 const server= JsonServer.create()
-const router= JsonServersonserver.router("db.json")
-const middlewares=JsonServersonServer.defaults()
+const router= JsonServer.router("db.json")
+const middlewares=JsonServer.defaults()
 const port = process.env.PORT || 3001 // you can use any port nater I chose to whe
 
 server.use(middlewares)
